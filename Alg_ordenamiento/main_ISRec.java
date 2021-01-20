8ed66b0f725210f58e562840f387022bb48207dc
haidejaquelineAF <66649588+haidejaquelineAF@users.noreply.github.com>
2021-01-19 20:45:16 -0600

    Create main_ISRec.java

---
+++ Alg_ordenamiento/main_ISRec.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Alg_ordenamiento;
+
+import busquedas.GeneradorDatos;
+
+import aa20201.data.Grafica;
+import java.util.Arrays;
+
+
+/**
+ *
+ * @author Haide
+ */
+public class main_ISRec {
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int n = 10000;
+        int[] aux;
+        int[] aux2;
+        int[] aux3;
+        double[] tiempos = new double[n];
+        double[] tiempos2 = new double[n];
+        double[] tiempos3 = new double[n];
+        InsertShort b = new InsertShort();
+        InsertShort b2 = new InsertShort();
+        InsertShort b3 = new InsertShort();
+       
+        for (int j = 0; j < n; j++){
+            aux = GeneradorDatos.generarArregloInt(j,1000);
+            aux2 = GeneradorDatos.ordenarDesc(aux);
+            aux3 = GeneradorDatos.ordenarAsc(aux);
+            b.ordenarIS(aux,aux.length);
+            b2.ordenarIS(aux2,aux2.length);
+            b3.ordenarIS(aux3,aux3.length);
+            System.out.println(Arrays.toString(aux));
+            System.out.println(Arrays.toString(aux2));
+            System.out.println(Arrays.toString(aux3));
+            tiempos[j] = (int) b.gettTotal();
+            tiempos2[j] = (int) b2.gettTotal();
+            tiempos3[j] = (int) b3.gettTotal();
+            
+            //System.out.println("Pj= "+j+" :"+tiempos[j]);
+            //System.out.println("Mj= "+j+" :"+tiempos2[j]);
+        }  
+        //System.out.println();
+        Grafica g1 = new Grafica("N","Tiempo","Tiempos");
+        g1.agregarSerie("Medio",tiempos);
+        g1.agregarSerie("Peor",tiempos2);
+        g1.agregarSerie("Mejor",tiempos3);
+        g1.crearGrafica();
+        g1.muestraGrafica();
+       
+       
+    }
+    
+}
