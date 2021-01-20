0eec8b41bfd936eb2573cf5e4d755409fa48cc44
haidejaquelineAF <66649588+haidejaquelineAF@users.noreply.github.com>
2021-01-19 21:08:43 -0600

    Create Ruta.java

---
+++ TSP/Ruta.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TSP;
+
+/**
+ *
+ * @author Haide
+ */
+public class Ruta {
+    public static void main(String[] args) {
+        Principal p = new Principal();
+        p.principal();
+    }
+}
